1c39857104f7674aaa6ecad5003677df210fb6aa
catsnipe <catsnipe@gmail.com>
2021-06-08 15:18:39 +0900

    first commit

---
+++ Assets/Plugins/uindies/data/jsonSaveLoad.cs
@@ -0,0 +1,63 @@
+﻿using System.IO;
+using UnityEngine;
+
+/// <summary>
+/// jsonSaveLoad
+/// : simple save/load(json format)
+/// </summary>
+public class jsonSaveLoad
+{
+    /// <summary>
+    /// save data
+    /// </summary>
+    /// <param name="filename"></param>
+    /// <param name="data"></param>
+    /// <returns>false..error</returns>
+    public static bool Save(string filename, object data)
+    {
+        string jsonData = JsonUtility.ToJson(data);
+        bool   success  = true;
+
+        using (StreamWriter writer = new StreamWriter(Path.Combine(Application.persistentDataPath, filename), false))
+        {
+            try
+            {
+                writer.Write(jsonData);
+                writer.Flush();
+            }
+            catch (System.Exception ex)
+            {
+                Debug.LogError(ex.Message);
+                success = false;
+            }
+        }
+
+        return success;
+    }
+
+    /// <summary>
+    /// load data
+    /// </summary>
+    /// <typeparam name="T">data class</typeparam>
+    /// <param name="filename"></param>
+    /// <returns>null..error</returns>
+    public static T Load<T>(string filename) where T : class
+    {
+        string data = null;
+
+        using (StreamReader reader = new StreamReader(Path.Combine(Application.persistentDataPath, filename), false))
+        {
+            try
+            {
+                data = reader.ReadToEnd();
+            }
+            catch (System.Exception ex)
+            {
+                Debug.LogError(ex.Message);
+                data = null;
+            }
+        }
+        
+        return data == null ? null : JsonUtility.FromJson<T>(data);
+    }
+}
---
+++ Assets/sample.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class sample : MonoBehaviour
+{
+    [SerializeField]
+    TextMeshProUGUI Text = null;
+
+    public enum eTest
+    {
+        No0,
+        No1,
+        No2,
+    }
+
+    [System.Serializable]
+    public class TestData
+    {
+        public int      ID;
+        public string   Message;
+        public bool     Flag;
+        public eTest    Test;
+        public int[]    Stage;
+    }
+
+    void Awake()
+    {
+        // clear
+        updateLoadData(new TestData());
+    }
+
+    void updateLoadData(TestData data)
+    {
+        Text.SetText(
+            $"ID = {data.ID}{Environment.NewLine}" +
+            $"Message = '{data.Message}'{Environment.NewLine}" +
+            $"Flag = {data.Flag}{Environment.NewLine}" +
+            $"Test = {data.Test}{Environment.NewLine}"
+        );
+    }
+
+    public void ClickButtonSave()
+    {
+        TestData savedata = new TestData()
+        {
+            ID = 10,
+            Message = "TestData",
+            Flag = true,
+            Test = eTest.No1,
+            Stage = new int[] { 1,2,3,4,5 },
+        };
+
+        jsonSaveLoad.Save("savedata", savedata);
+    }
+
+    public void ClickButtonLoad()
+    {
+        TestData loaddata = jsonSaveLoad.Load<TestData>("savedata");
+
+        // display load data
+        updateLoadData(loaddata);
+    }
+}
